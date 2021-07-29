e971d6326906ffd8a9785f61e18ef754afc95c92
CanRod <candida2295@gmail.com>
2021-07-30 00:30:54 +0530

    expense item added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,20 @@ import ExpenseItem from './components/ExpenseItem';
 
 
 function App() {
+
+  const expenses =[
+    {date : new Date (2021,7,28),title : 'Shopping',amount: 2000},
+    {date : new Date (2021,7,29),title : 'Lunch',amount:500},
+    {date : new Date (2021,7,24),title : 'Veggies',amount: 200}
+
+  ];
+
   return (
     <div className="App">
       <Navbar></Navbar>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem title ={expenses[0].title} date ={expenses[0].date} amount ={expenses[0].amount} ></ExpenseItem>
+      <ExpenseItem title ={expenses[1].title} date ={expenses[1].date} amount ={expenses[1].amount} ></ExpenseItem>
+      <ExpenseItem title ={expenses[2].title} date ={expenses[2].date} amount ={expenses[2].amount} ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,23 @@
 import './ExpenseItem.css';
 
-function ExpenseItem ()
-{
+function ExpenseItem (props)
+{   
+    const year =props.date.getFullYear();
+    const month=props.date.toLocaleString('en-US',{month:'long'});
+    const day =props.date.toLocaleString('en-US',{day:'2-digit'});
 
     return(
-    <div className="expense-item" style ={{
-       // borderRadius: "8px"
-    }}>
-        <div>Date</div>
+    <div className="expense-item">
+
+        <div>
+            <div>{year}</div>
+            <div>{month}</div>
+            <div>{day}</div>
+        </div>
+
         <div className ="expense-item__description">
-            <h2>Title</h2>
-            <div className="expense-item__price">Amount</div>
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
         </div>
     </div>
     )
